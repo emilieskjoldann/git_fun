fd60329adaa32c68905a39b5d4f445a85b6cbd5b
emilie <emjo0001@stud.kea.dk>
2023-08-24 10:53:38 +0200

    hej

---
+++ js.js
@@ -0,0 +1,6 @@
+const info = document.querySelector("#info");
+console.log("info load");
+info.textContent += "HALLO";
+
+const article = document.querySelector("article:nth-child(2)");
+console.log(article);
